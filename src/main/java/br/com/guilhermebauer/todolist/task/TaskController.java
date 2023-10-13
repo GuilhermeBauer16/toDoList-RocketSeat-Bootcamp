b23e878391c1661e23565fbd6cb6b610811f1ee2
Guilherme <guilhermebauer16@gmail.com>
2023-10-13 19:16:05 -0300

    feat: completed the lesson 5, dealing with exceptions

--- src/main/java/br/com/guilhermebauer/todolist/task/TaskController.java
+++ src/main/java/br/com/guilhermebauer/todolist/task/TaskController.java
@@ -47,12 +47,25 @@ public class TaskController {
 
     @PutMapping("/{id}")
     @Transactional
-    public TaskModel update(@RequestBody TaskModel taskModel , HttpServletRequest request , @PathVariable UUID id){
-        var idUser = request.getAttribute("idUser");
+    public ResponseEntity update(@RequestBody TaskModel taskModel , HttpServletRequest request , @PathVariable UUID id){
+
         var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null){
+            return ResponseEntity.badRequest().body("The task has not been found or does not exist");
+        }
+        var idUSer = request.getAttribute("idUser");
+
+        if (!task.getUserId().equals(idUSer)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("The user doesn't have authorization to alter that task.");
+
+        }
+
         Utils.copyNonNullProperties(taskModel , task);
 
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
 
     }
 }
--- src/main/java/br/com/guilhermebauer/todolist/task/TaskModel.java
+++ src/main/java/br/com/guilhermebauer/todolist/task/TaskModel.java
@@ -30,4 +30,11 @@ public class TaskModel {
     private LocalDateTime createdAt;
 
     private UUID userId;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50){
+            throw new Exception("The title field shouldn't more that 50 characters in length.");
+        }
+        this.title = title;
+    }
 }
